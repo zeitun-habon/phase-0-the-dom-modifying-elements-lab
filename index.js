12f22a7e9daf24d19cb0f2c90cfe3232180d013d
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-12 20:04:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Newton is the champion";
+newHeader.className = "victorious";
